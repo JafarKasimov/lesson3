948755d755e68d545c6c3c27bb13b7319c12a9fd
Jafar Kasymov <116582499+JafarKasimov@users.noreply.github.com>
2022-10-26 18:22:40 +0600

    asfasdfasd

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        years(generateRandomAge(), 542);
-        years(generateRandomAge(), -50);
-        years(generateRandomAge(), 10);
-        years(generateRandomAge(), 2);
-        years(generateRandomAge(), 33);
-
-
+        System.out.println(years(generateRandomAge(), 542));
+        System.out.println(years(generateRandomAge(), -50));
+        System.out.println(years(generateRandomAge(), 10));
+        System.out.println(years(generateRandomAge(), 10));
+        System.out.println(years(generateRandomAge(), 10));
     }
 
-        private static int years (int oldd, int degree) {
+        private static String years (int oldd, int degree) {
 
             if (oldd >= 20 && oldd <= 45 && degree > -20 && degree < 30)
-                System.out.println("можно1 ли идти гулять");
+                return ("можно1 ли идти гулять");
 
             else if (oldd < 20 && degree >= 0 && degree <= 28)
-                System.out.println("можно2 идти гулять");
+                return ("можно2 идти гулять");
             else if (oldd > 45 && degree > -10 && degree < 25) {
-                System.out.println("можно3 идти гулять");
+                return ("можно3 идти гулять");
             } else {
-                System.out.println("оставайтесь дома");
+                return ("оставайтесь дома");
+
             }
-            return oldd+degree;
-        }
+     }
 
 
     public static int generateRandomAge() {
